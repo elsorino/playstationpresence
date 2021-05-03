08dc8d8ded874812a7351a7ae9471d880e7ce633
elsorino <elso@pm.me>
2021-05-03 16:16:10 -0400

    added function for rpc

--- playstationpresence.py
+++ playstationpresence.py
@@ -4,6 +4,12 @@ import configparser
 import time
 import ast
 
+def discordrpc(appid):
+    global rpc
+    rpc.clear()
+    rpc = Presence(appid, pipe=0) #PS4 games on PS5
+    rpc.connect()
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 npsso = config['main']['npsso']
@@ -13,8 +19,8 @@ psnawp = psnawp.PSNAWP(npsso)
 start_time = int(time.time())
 oldpresence = ""
 #Initial usage, used to clear status if user is offline
-RPC = Presence("829124881324048404",pipe=0)
-RPC.connect()
+rpc = Presence("829124881324048404",pipe=0)
+rpc.connect()
 
 while True:
     user_online_id = psnawp.user(online_id=PSNID)
@@ -23,30 +29,24 @@ while True:
     start_time = int(time.time())
     if 'offline' in mainpresence:
         print("User is offline, clearing status")
-        RPC.clear()
+        rpc.clear()
     else: 
         if (oldpresence == mainpresence):
             pass
         else:
-            #Best way to work with backwards compatability is a seprate RPClient named Playstation 5 with PS4 game assets
+            #Best way to work with backwards compatability is a seprate rpclient named Playstation 5 with PS4 game assets
             if 'PS5' in mainpresence:
                 system = "ps5"
                 if 'CUSA' in mainpresence:
-                    RPC.clear()
-                    RPC = Presence("829746683835187220", pipe=0) #PS4 games on PS5
-                    RPC.connect()
+                    discordrpc("829746683835187220") #PS4 games on PS5
                 else:
-                    RPC.clear()
-                    RPC = Presence("829547127809638451",pipe=0) #PS5 games
-                    RPC.connect()
+                    discordrpc("829547127809638451") #PS5 games
             else:
                 system = "ps4"
-                RPC.clear()
-                RPC = Presence("829124881324048404",pipe=0) #PS4 system
-                RPC.connect()
+                discordrpc("829124881324048404")
             current = mainpresence.split("'")
             if (len(current) == 19): #Length of this is 19 if user is not in a game
-                RPC.update(state="Idling", start=start_time, small_image=system, small_text=system, large_image=system, large_text="Homescreen")
+                rpc.update(state="Idling", start=start_time, small_image=system, small_text=system, large_image=system, large_text="Homescreen")
                 print("Idling")
             else:
                 if 'gameStatus' in mainpresence: #Not every game supports gameStatus
@@ -56,7 +56,7 @@ while True:
                 gameid = current[23]
                 gamename = current[27]
                 #gamestatus = current[]
-                RPC.update(state=gamename, start=start_time, small_image=system, small_text=system, large_image=gameid.lower(), large_text=gametext)
+                rpc.update(state=gamename, start=start_time, small_image=system, small_text=system, large_image=gameid.lower(), large_text=gametext)
                 print("Playing %s" %gamename)
     time.sleep(15) #Adjust this to be higher if you get ratelimited
     oldpresence = mainpresence
